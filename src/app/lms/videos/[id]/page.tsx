5bd27aa36eba28e93da7ed916ccf09b4c268746d
unknown <rai.akash667@gmail.com>
2025-09-23 16:32:23 +0530

    video page edit

--- src/app/lms/videos/[id]/page.tsx
+++ src/app/lms/videos/[id]/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
         <div className="trezo-card-content">
           <Nav />
 
-          <AccountSettingsForm />
+          {/* <AccountSettingsForm /> */}
         </div>
       </div>
     </>
